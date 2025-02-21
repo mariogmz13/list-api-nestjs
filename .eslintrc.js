3e0e8d8ac71b44a6b8cdb9e93c4c1d9e2e19825c
mega <mariogamez1999@gmail.com>
2025-02-21 10:55:40 -0700

    eslint desactivado

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,4 +22,11 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
+  //desactivar eslint
+  "prettier/prettier": [
+  "error",
+  {
+    "endOfLine": "auto"
+  }
+]
 };
